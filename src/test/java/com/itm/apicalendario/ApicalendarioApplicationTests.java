c6a1ab1bd0d2d8744e607bb886254d213a2c5c96
sebastian <scastadi92@hotmail.com>
2026-03-02 21:18:34 -0500

    Proyecto inicial Api Calendario

---
+++ src/test/java/com/itm/apicalendario/ApicalendarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itm.apicalendario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicalendarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
